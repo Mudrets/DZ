82ca32bf0cfeb365a5b0d27bdd3f28089d8bbcd1
Mudrets <mudrets675@gmail.com>
2023-11-12 20:34:46 +0200

    13.11.23

--- DZ32.js
+++ DZ32.js
@@ -4,65 +4,69 @@ const Stand = [
         ReliseDate: 2013,   //дата релиза пары кроссовок
         Edition: 100000,    //количество произведённых пар кроссовок
         RetailPrise: 180,   //цена производителя
-        ResailPrise: 350    //цена перепродажи
+        ResailPrise: 350,   //цена перепродажи
+        img:`<img src="https://restocks.net/_next/image/?url=https%3A%2F%2Fcdn.restocks.net%2Fcdn-cgi%2Fimage%2Fwidth%3D1000%2Fstorage%2Fimages%2Fproducts%2F845035-003%2F1.png&w=3840&q=75" alt=""></img>`
     },
     {
         Pair: 'Air Jordan 1',
         ReliseDate: 1985,
         Edition: 2500000,
         RetailPrise: 120,
-        ResailPrise: 1900
+        ResailPrise: 1900,
+        img:`<img src="https://cdn.shopify.com/s/files/1/2358/2817/products/air-jordan-1-high-chicago-lost-and-found-1.png?v=1669025825" alt=""></img>`
     },
     {
         Pair: 'Air Jordan 4',
         ReliseDate: 2022,
         Edition: 30000,
         RetailPrise: 220,
-        ResailPrise: 450
+        ResailPrise: 450,
+        img:`<img src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2023%2F05%2Fair-jordan-4-frozen-times-aq9129-001-release-date-0-1.jpg?fit=max&cbr=1&q=90&w=750&h=500" alt=""></img>`
     },
     {
         Pair: 'Air Jordan 3',
         ReliseDate: 2017,
         Edition: 150000,
         RetailPrise: 180,
-        ResailPrise: 220
+        ResailPrise: 220,
+        img:`<img src="https://image-cdn.hypb.st/https%3A%2F%2Fhypebeast.com%2Fimage%2F2022%2F11%2Fair-jordan-3-wizards-pe-ct8532-148-release-info-tw0.jpg?w=960&cbr=1&q=90&fit=max" alt=""></img>`
     },
     {
         Pair: 'Air Jordan 6',
         ReliseDate: 2021,
         Edition: 15000,
         RetailPrise: 180,
-        ResailPrise: 650
+        ResailPrise: 650,
+        img:`<img src="https://flowerkickz.gr/cdn/shop/products/flowerkickzAir_Jordan_6_Retro_Carmine_2021_--0.png?v=1646149319" alt=""></img>`
     },
     {
         Pair: 'Air Jordan 11',
         ReliseDate: 1996,
         Edition: 60000,
         RetailPrise: 180,
-        ResailPrise: 930
+        ResailPrise: 930,
+        img:`<img src="https://izicop.com/cdn/shop/products/air-jordan-11-bred-83685-1-1000.png?v=1645033800" alt=""></img>`
     },
 
 ]
 const login = document.querySelector(`.ins`);
 const deleter = document.querySelector(`.delete`)
 function log(){
-    document.querySelector(`.ins`).innerHTML = `<div class = "col-2 commit golova">Номер</div>
-    <div class = "col-2 commit golova">Пара</div>
-    <div class = "col-2 commit golova">Дата релиза</div>
-    <div class = "col-2 commit golova">Сток</div>
-    <div class = "col-2 commit golova">Ретеил</div>
-    <div class = "col-2 commit golova">Рессеил</div>`
+    document.querySelector(`.ins`).innerHTML = ``
     for(i=0;i < Stand.length; i++){
         login.insertAdjacentHTML(
             "beforeend",
-            `
-                <div class = "col-2 commit">${i+1}</div>
-                <div class = "col-2 commit">${Stand[i].Pair}</div>
-                <div class = "col-2 commit">${Stand[i].ReliseDate}</div>
-                <div class = "col-2 commit">${Stand[i].Edition}</div>
-                <div class = "col-2 commit">${Stand[i].RetailPrise}</div>
-                <div class = "col-2 commit">${Stand[i].ResailPrise}</div>
-            `
+            `   <div class = "col-4"><div class = "block ">
+                <div class = "number">${i+1}</div>
+                <div class = "para">${Stand[i].Pair}</div>
+                <div class = "img">${Stand[i].img}</div>
+                <div class = "relise">Дата релиза: <span class = "prise">${Stand[i].ReliseDate} год</span></div>
+                <div class = "stok">Сток: <span class = "prise">${Stand[i].Edition} пар</span></div>
+                <div class = "reteil">Ретеил: <span class = "prise">${Stand[i].RetailPrise}$</div>
+                <div class = "reseil">Ресеил: <span class = "prise">${Stand[i].ResailPrise}$</span></div>
+                </div>
+                </div>
+                `
         )
     }
 }
@@ -124,11 +128,11 @@ function Adder(Pair,RD,Edition,RetPr,ResPr){
 
 
 function AddPair(){
-    const Pairs = document.querySelector(`.Pairs`).value
-    const RD = document.querySelector(`.RD`).value
-    const Edition = document.querySelector(`.Edition`).value
-    const RetPrise = document.querySelector(`.RetPrise`).value
-    const ResPrise = document.querySelector(`.ResPrise`).value
+    const Pairs = document.querySelector(`.k1`).value
+    const RD = document.querySelector(`.k2`).value
+    const Edition = document.querySelector(`.k3`).value
+    const RetPrise = document.querySelector(`.k4`).value
+    const ResPrise = document.querySelector(`.k5`).value
     if (Pairs === ``){
         document.querySelector(`.zam`).innerHTML = `<div class = "commit red zam">Укажите название пары</div>`
         return
