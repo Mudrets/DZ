58875154993842da18eff3c405864b2a5c1bb62e
Mudrets <mudrets675@gmail.com>
2024-03-04 18:25:43 +0200

    ts_3

--- app.js
+++ app.js
@@ -1,59 +1,40 @@
 "use strict";
-//1
-console.log('1');
-let user = ["Johan", 25];
-console.log(`Name: ${user[0]}, Age: ${user[1]}`);
-//2
-console.log('');
-console.log('2');
-const genders = [
-    { id: 1, gender: 'men' },
-    { id: 2, gender: 'women' },
-    { id: 3, gender: 'men' },
-    { id: 4, gender: 'men' },
-];
-function searchAll(mass, gender) {
-    const filteredMass = mass.filter(obj => obj.gender === gender);
-    console.log(filteredMass);
+function createShape(name, calculateArea) {
+    return {
+        name,
+        calculateArea,
+    };
 }
-searchAll(genders, 'men');
-searchAll(genders, 'women');
-//3
-console.log('');
-console.log('3');
-const vegetables = [
-    { id: 1, vegetable: 'tomato' },
-    { id: 2, vegetable: 'potato' },
-    { id: 3, vegetable: 'paper' },
-    { id: 4, vegetable: 'cucumber' },
-    { id: 5, vegetable: 'paper' },
-];
-function searchFirst(mass, gender) {
-    const filteredMass = mass.find(obj => obj.vegetable === gender);
-    if (filteredMass) {
-        console.log(filteredMass);
-    }
-    else {
-        console.log('Gender not found');
-    }
+function calculateCircleArea(radius) {
+    return Math.PI * Math.pow(radius, 2);
 }
-searchFirst(vegetables, 'paper');
-searchFirst(vegetables, 'banana');
-//4
-console.log('');
-console.log('4');
-const digits = [
-    { id: 1, digit: 21 },
-    { id: 2, digit: 43 },
-    { id: 3, digit: 2 },
-    { id: 4, digit: 11 },
-    { id: 5, digit: 96 },
-];
-function average(mass, digit) {
-    const sum = mass.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue[digit];
-    }, 0);
-    const average = sum / mass.length;
-    return average;
+function calculateRectangleArea(width, height) {
+    return width * height;
 }
-console.log(average(digits, 'digit'));
+const circle = createShape("Circle", () => calculateCircleArea(5));
+console.log(`S ${circle.name}: ${circle.calculateArea()}`);
+const rectangle = createShape("Rectangle", () => calculateRectangleArea(4, 6));
+console.log(`S ${rectangle.name}: ${rectangle.calculateArea()}`);
+function updateSettings(newSettings) {
+    console.log("New Settings:", newSettings);
+}
+const newSettings = {
+    theme: "light",
+    fontSize: 16,
+    isDarkMode: false,
+};
+updateSettings(newSettings);
+function getUserProfile(user, account) {
+    const userProfile = Object.assign(Object.assign({}, user), account);
+    return userProfile;
+}
+const user = {
+    name: "Name",
+    age: 25,
+};
+const account = {
+    username: "UserName",
+    email: "rendom@example.com",
+};
+const userProfileResult = getUserProfile(user, account);
+console.log("UserProfile res:", userProfileResult);
--- app.ts
+++ app.ts
@@ -1,65 +1,78 @@
-interface mass {
-    [key: string]: any;
-}
 //1
-console.log('1');
-type tuple = [string, number];
+interface Shape {
+    name: string;
+    calculateArea: () => number;
+  }
+  
+  function createShape(name: string, calculateArea: () => number): Shape {
+    return {
+        name,
+        calculateArea,
+    };
+}
+function calculateCircleArea(radius: number): number {
+    return Math.PI * Math.pow(radius, 2);
+}
+function calculateRectangleArea(width: number, height: number): number {
+    return width * height;
+}
+const circle = createShape("Circle", () => calculateCircleArea(5));
+console.log(`S ${circle.name}: ${circle.calculateArea()}`);
 
-let user: tuple = ["Johan", 25];
-
-console.log(`Name: ${user[0]}, Age: ${user[1]}`);
+const rectangle = createShape("Rectangle", () => calculateRectangleArea(4, 6));
+console.log(`S ${rectangle.name}: ${rectangle.calculateArea()}`);
 
 //2
-console.log('');
-console.log('2');
-const genders:mass[] =[
-    {id:1,gender:'men'},
-    {id:2,gender:'women'},
-    {id:3,gender:'men'},
-    {id:4,gender:'men'},
-]
-function searchAll (mass:mass[],gender:string):mass{
-    const filteredMass = mass.filter(obj => obj.gender === gender)
-
-    console.log(filteredMass);
-}
-searchAll(genders,'men')
-searchAll(genders,'women')
-
-//3
-console.log('');
-console.log('3');
-const vegetables:mass[] =[
-    {id:1,vegetable:'tomato'},
-    {id:2,vegetable:'potato'},
-    {id:3,vegetable:'paper'},
-    {id:4,vegetable:'cucumber'},
-    {id:5,vegetable:'paper'},
-]
-function searchFirst (mass:mass[],gender:string):mass{
-    const filteredMass = mass.find(obj => obj.vegetable === gender)
-    if(filteredMass){
-        console.log(filteredMass);
-    }else{console.log('Gender not found')}
-}
-searchFirst(vegetables,'paper')
-searchFirst(vegetables,'banana')
-
-//4
-console.log('');
-console.log('4');
-const digits:mass[] =[
-    {id:1,digit:21},
-    {id:2,digit:43},
-    {id:3,digit:2},
-    {id:4,digit:11},
-    {id:5,digit:96},
-]
-function average (mass:mass[],digit:string):number{
-    const sum: number = mass.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue[digit];
-    }, 0);
-    const average = sum / mass.length
-    return average
-}
-console.log(average(digits,'digit'));
+interface Settings {
+    theme?: string;
+    fontSize?: number;
+    isDarkMode?: boolean;
+  }
+  
+  function updateSettings(newSettings: Settings): void {
+    console.log("New Settings:", newSettings);
+  }
+  
+  const newSettings: Settings = {
+    theme: "light",
+    fontSize: 16,
+    isDarkMode: false,
+  };
+  
+  updateSettings(newSettings);
+  
+  //3
+  interface User {
+    name: string;
+    age: number;
+  }
+  
+  interface Account {
+    username: string;
+    email: string;
+  }
+  
+  interface UserProfile extends User, Account {}
+  
+  function getUserProfile(user: User, account: Account): UserProfile {
+    const userProfile: UserProfile = {
+      ...user,
+      ...account,
+    };
+  
+    return userProfile;
+  }
+  
+  const user: User = {
+    name: "Name",
+    age: 25,
+  };
+  
+  const account: Account = {
+    username: "UserName",
+    email: "rendom@example.com",
+  };
+  
+  const userProfileResult: UserProfile = getUserProfile(user, account);
+  console.log("UserProfile res:", userProfileResult);
+  
